7e3a9a7c8959190759d1c8e5ebda7857d7231c42
DianiniDeluca <144449327+DianiniDeluca@users.noreply.github.com>
2023-10-25 08:39:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+/** soma*/ 
+let a = 5;
+let b =6
+function somar(a,b){
+   return a +b;
+}
+let valores = somar(a,b);
+  console.log(valores)
+
+/**subtração */
+let sub1 =6;
+let sub2 = 8;
+function subtrair ( sub1 , sub2){
+    return sub1 - sub2
+}
+let  resul = subtrair(sub1 , sub2)
+console.log(resul)
+
+/**multiplicação*/
+let e = 32 ;
+let f = 2;
+function multiple(e,f){
+    return e * f;
+}
+let calc = multiple(e,f)
+console.log(calc)
+
+  /**divisao */
+let div1 = 3;
+let div2 = 9;
+function division(div1, div2){
+    return div1 / div2
+}let dividi = division(div1, div2)
+console.log(dividi)
+
+/**modulo */
+let mod1 = 30;
+let mod2 = 4;
+function modo(mod1, mod2){
+    return mod1 % mod2;
+}let modelo = modo(mod1, mod2)
+console.log(modelo)
+
+/**concatenar */
+let conc ="a";
+let conca = 5;
+function concate( conc,conca){
+    return conc + conca
+}let juntar = concate(conc, conca)
+console.log(juntar)
+
